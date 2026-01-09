3364069517064d57122dcb0d86a4fd988699c4a1
mudasirhayat <mudasirhayat5@gmail.com>
2026-01-09 09:00:09 +0000

    fix: improve error handling

--- scripts/generate-tray-icon.py
+++ scripts/generate-tray-icon.py
@@ -59,7 +59,8 @@ except ValueError as e:
 try:
     os.makedirs('../electron-app/assets')
 except Exception as e:
-    print(f"An error occurred: {e}")
+    logging.error(f"An error occurred: {e}")
+    raise
     pass
 import logging
 
